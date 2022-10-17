e727880657743dba6b998e3006cadb56f94034a7
Alesia <alesia.litvinenko@gmail.com>
2022-10-17 10:43:05 +0300

    feat: stack

--- src/stack.js
+++ src/stack.js
@@ -13,21 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = [];
+  }
+  push(element) {
+    this.stack.push(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    let popStack = this.stack.pop();
+    return popStack
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let lengthStack = this.stack.length;
+    return this.stack[lengthStack - 1];
   }
 }
+
 
 module.exports = {
   Stack
