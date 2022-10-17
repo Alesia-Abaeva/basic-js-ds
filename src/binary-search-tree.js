43f1221bc61681d40b31d0a654bdde73d0e831d6
Alesia <alesia.litvinenko@gmail.com>
2022-10-17 23:02:59 +0300

    feat: binary remove

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -88,39 +88,39 @@ class BinarySearchTree {
 
 // const values = [... new Set([...Array(5)].map(_=>Math.ceil(Math.random()*100)))]
 
-const values = [72, 25, 22, 41, 13]
-const sortedValues = [
-  25, 41
-]
-const valuesToRemove = [
-  22, 72, 13
-]
+// const values = [72, 25, 22, 41, 13]
+// const sortedValues = [
+//   25, 41
+// ]
+// const valuesToRemove = [
+//   22, 72, 13
+// ]
 
-console.log('initial',values)
-const tree = new BinarySearchTree();
-values.forEach((value) => tree.add(value));
-// values.sort((a, b) => (a % 2) - (b % 2) || a - b);
-// console.log('sorted values',values)
-// const valuesToRemove = values.splice(0, 3);
-// console.log('valuesToRemove',valuesToRemove)
-valuesToRemove.forEach((value, index) => {
-  // if (index > 1) {
-  //   return
-  // }
+// console.log('initial',values)
+// const tree = new BinarySearchTree();
+// values.forEach((value) => tree.add(value));
+// // values.sort((a, b) => (a % 2) - (b % 2) || a - b);
+// // console.log('sorted values',values)
+// // const valuesToRemove = values.splice(0, 3);
+// // console.log('valuesToRemove',valuesToRemove)
+// valuesToRemove.forEach((value, index) => {
+//   // if (index > 1) {
+//   //   return
+//   // }
 
-  tree.remove(value)
-});
+//   tree.remove(value)
+// });
 
-valuesToRemove.forEach((value) => {
-  console.log(`has ${value}`, tree.has(value));
-});
-console.log("all false");
-sortedValues.forEach((value) => {
-  console.log(`has ${value}`, tree.has(value));
-  tree.has(value);
-});
-console.log("all true");
-console.log(tree)
+// valuesToRemove.forEach((value) => {
+//   console.log(`has ${value}`, tree.has(value));
+// });
+// console.log("all false");
+// sortedValues.forEach((value) => {
+//   console.log(`has ${value}`, tree.has(value));
+//   tree.has(value);
+// });
+// console.log("all true");
+// console.log(tree)
 module.exports = {
   BinarySearchTree,
 };
